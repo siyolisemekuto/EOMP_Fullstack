07c36769a41bf48b6e594ea2fc2f47c5664b34d9
unknown <siyozamekuto2.0@gmail.com>
2022-08-06 13:28:23 +0200

    footer completed

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <footer>
+            <span>
+                Copyright &copy;2022, All rights reserved. 
+                <!-- link to githubnpm -->
+                <a href= https://github.com/siyolisemekuto/EOMP_Fullstack target='_blank'> 
+                Made by Live Maweni and Siyolise Mekuto.
+                </a>
+            </span>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+footer {
+  text-align: center;
+  /* padding: 3px; */
+  background-color: #141414;
+  color: white;
+  display: block;
+  height: 70px;
+  display:flex;  
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+}
+a{
+  text-decoration: none;
+  color: white;
+}
+a:hover{
+    color: #C7A17F;
+}
+</style>
